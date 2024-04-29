18b6554a607ca9c7bb8414fa2e406389e3d20687
AnthonyTrindade21 <anthonyfranca11@gmail.com>
2024-04-29 19:01:30 -0300

    atividade de innertext e innerhtml

---
+++ script.js
@@ -0,0 +1,28 @@
+
+titulo.innerText = "Novo Titulo";
+console.log(titulo.innerText)
+
+let link = document.querySelector("a")
+link.innerText = "link para o site Proz"
+console.log(link.innerText)
+
+listanaoordenada.innerHTML = `
+<ul>
+<li>lista</>
+<li>não</li>
+<li>ordenada</li>
+</ul>
+`
+console.log(listanaoordenada.innerHTML) 
+
+listaordenada.innerHTML = `
+<ol>
+<a href= "https://www.youtube.com/">youtube</a>
+<a href= "https://www.google.com.br/?hl=pt-BR">google</a>
+<a href= "https://pt-br.facebook.com/">facebook</a>
+</ol>
+`
+console.log(listaordenada.innerHTML)
+
+
+
